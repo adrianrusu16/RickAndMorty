d35af7d4fca5402d1ef609c51aa10f7c4609ffd0
adrianrusu16 <72539388+adrianrusu16@users.noreply.github.com>
2021-12-07 15:59:51 +0200

    no clue

---
+++ RickAndMorty/app/src/main/java/com/example/rickandmorty/api/CharacterApi.kt
@@ -0,0 +1,16 @@
+package com.example.rickandmorty.api
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface CharacterApi {
+
+    companion object {
+        const val BASE_URL = "https://rickandmortyapi.com/api/"
+    }
+
+    @GET("character")
+    suspend fun searchCharacters(
+        @Query("page") page: Int,
+    ): CharacterResponse
+}
